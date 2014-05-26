daffc0db7caa1acb9078670b4b68b7f646ed1a35
Trond Trosterud <trond.trosterud@uit.no>
2014-05-26 06:49:00 +0000

    update

--- scripts/c2h.pl
+++ scripts/c2h.pl
@@ -10,14 +10,16 @@ print STDOUT "<html><body>\n";
 while (<>) 
 {
 	chomp ;
-	my ($lemma, $lemma2, $var, $CG, $POS, $trans, $trans2, $syn, $ex1, $extrans1, $ex2, $extrans2, $ex3, $extrans3, $ex4, $extrans4) = split /\t/ ;
+	#     a      b       c     d      e      f        g     h      i         j      k         l       m     n          o      p 
+	my ($lemma, $lemma2, $var, $CG,  $POS, $trans, $trans2, $syn, $ex1, $extrans1, $ex2, $extrans2, $ex3, $extrans3, $ex4, $extrans4) = split /\t/ ;
 	print STDOUT "  <p>\n";
 	print STDOUT "  <div style=\"margin-left:1.1em;text-indent:-1.1em\"><b>$lemma $lemma2 $var $CG</b>\n";
 	print STDOUT "    <i>$POS</i>\n";
 	print STDOUT "     ▸ $trans $trans2;\n";
 	print STDOUT "     <b>$syn $ex1</b> $extrans1\n";
-	print STDOUT "     <b>$ex2</b> $extrans2\n";
-	print STDOUT "     <b>$ex3</b> $extrans3\n";
+	print STDOUT "     <b>$ex2</b> $extrans2, \n";
+	print STDOUT "     <b>$ex3</b> $extrans3, \n";
+	print STDOUT "     <b>$ex4</b> $extrans4\n";
 	print STDOUT "  </div>\n";
 	print STDOUT "  </p>\n";
 }
